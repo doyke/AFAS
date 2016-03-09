2e9a62e87a07865616042caedf5294e424a1aec3
Paveen Juntama <paveenju@icloud.com>
2016-03-09 16:33:30 +0700

    Added back-end projects

---
+++ EFX.Core/IRepository_T.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace EFX.Core
+{
+    public interface IRepository<T> : IRepository, IDisposable where T : class
+    {
+        IQueryable<T> Query(Func<T, bool> predicate);
+        T Add(T item);
+        T Remove(T item);
+        int SaveChanges();
+    }
+}
---
+++ EFX.Core/IService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace EFX.Core
+{
+    public interface IService
+    {
+        RootClass Root { get; set; }
+        DbContext Context { get; set; }
+    }
+}
